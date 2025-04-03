52b35f7eb7e2ba2dfe886d61d28144718c2e9a54
DFCH9503 <dafechaparro@hotmail.com>
2025-04-03 11:54:45 -0500

    opening repository

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.scss";
+
+const Header = () => (
+    <div className="header">
+        <h2>Realtime Chat App by DFCH9503</h2>
+    </div>
+);
+
+export default Header;
