62c972401950dec953fdadc19e09f36cf1ccb78a
DFCH9503 <dafechaparro@hotmail.com>
2025-04-04 11:40:57 -0500

    finishing multiple-clients

--- backend/main.go
+++ backend/main.go
@@ -8,29 +8,33 @@ import (
 )
 
 
-// define our WebSocket endpoint
-func serveWs(w http.ResponseWriter, r *http.Request) {
-    fmt.Println(r.Host)
-
-  // upgrade this connection to a WebSocket
-  // connection
-    ws, err := websocket.Upgrade(w, r)
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+    fmt.Println("WebSocket Endpoint Hit")
+    conn, err := websocket.Upgrade(w, r)
     if err != nil {
         fmt.Fprintf(w, "%+v\n", err)
-	}
-  // listen indefinitely for new messages coming
-  // through on our WebSocket connection
-    go websocket.Writer(ws)
-    websocket.Reader(ws)
+    }
+
+    client := &websocket.Client{
+        Conn: conn,
+        Pool: pool,
+    }
+
+    pool.Register <- client
+    client.Read()
 }
 
 func setupRoutes() {
-  // mape our `/ws` endpoint to the `serveWs` function
-    http.HandleFunc("/ws", serveWs)
+    pool := websocket.NewPool()
+    go pool.Start()
+
+    http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+        serveWs(pool, w, r)
+    })
 }
 
 func main() {
-    fmt.Println("Chat App v0.02")
+    fmt.Println("Chat App v0.03")
     fmt.Println("Check server http://localhost:8080/")
     setupRoutes()
     http.ListenAndServe(":8080", nil)
