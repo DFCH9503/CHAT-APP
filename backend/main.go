44e3ff45f4e7d909b31ac65219c71fec32cd9256
DFCH9503 <dafechaparro@hotmail.com>
2025-04-04 11:11:30 -0500

    pkg/websocket ok in main.go

--- backend/main.go
+++ backend/main.go
@@ -2,41 +2,11 @@ package main
 
 import (
     "fmt"
-    "log"
     "net/http"
 
-    "github.com/gorilla/websocket"
+    websocket"package/websocket"
 )
 
-//UPGRADER to apply the /ws endpoint
-var upgrader = websocket.Upgrader{
-    ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	//return true allows the checking from any connection
-  CheckOrigin: func(r *http.Request) bool { return true },
-}
-
-// define a reader which will listen for
-// new messages being sent to our WebSocket
-// endpoint
-func reader(conn *websocket.Conn) {
-    for {
-    // read in a message
-        messageType, p, err := conn.ReadMessage()
-        if err != nil {
-            log.Println(err)
-            return
-        }
-    // print out that message for clarity
-        fmt.Println(string(p))
-
-        if err := conn.WriteMessage(messageType, p); err != nil {
-            log.Println(err)
-            return
-        }
-
-    }
-}
 
 // define our WebSocket endpoint
 func serveWs(w http.ResponseWriter, r *http.Request) {
@@ -44,25 +14,24 @@ func serveWs(w http.ResponseWriter, r *http.Request) {
 
   // upgrade this connection to a WebSocket
   // connection
-    ws, err := upgrader.Upgrade(w, r, nil)
+    ws, err := websocket.Upgrade(w, r)
     if err != nil {
-        log.Println(err)
+        fmt.Fprintf(w, "%+v\n", err)
 	}
   // listen indefinitely for new messages coming
   // through on our WebSocket connection
-    reader(ws)
+    go websocket.Writer(ws)
+    websocket.Reader(ws)
 }
 
 func setupRoutes() {
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Simple Server")
-})
   // mape our `/ws` endpoint to the `serveWs` function
     http.HandleFunc("/ws", serveWs)
 }
 
 func main() {
-    fmt.Println("Chat App v0.01")
+    fmt.Println("Chat App v0.02")
+    fmt.Println("Check server http://localhost:8080/")
     setupRoutes()
     http.ListenAndServe(":8080", nil)
 }
