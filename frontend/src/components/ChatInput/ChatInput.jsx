85a119becf09f0d5ee5f5a7a29efa7af963aa4b3
DFCH9503 <dafechaparro@hotmail.com>
2025-04-29 10:17:17 -0500

    front-loss

--- frontend/src/components/ChatInput/ChatInput.jsx
+++ frontend/src/components/ChatInput/ChatInput.jsx
@@ -1,5 +1,20 @@
 import React, { Component } from "react";
 import "./ChatInput.scss";
+import React, { useState } from 'react';
+
+// function ChatInput() {
+//     const [message, setMessage] = useState('');
+
+//     return (
+//         <input
+//         type="text"
+//         placeholder="Type a message..."
+//         value={message}
+//         onChange={(e) => setMessage(e.target.value)}
+//         className="p-2 border rounded w-full"
+//         />
+//     );
+//     }
 
 class ChatInput extends Component {
     render() {
