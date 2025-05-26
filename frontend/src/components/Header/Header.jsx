b0fb17ba27f5c372a0651071870c99d7ec3e7d85
DFCH9503 <dafechaparro@hotmail.com>
2025-05-26 18:51:55 -0500

    changing-style-1

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import "./Header.scss";
+import { FaGithub } from 'react-icons/fa';
 
 const Header = () => (
     <div className="header">
         <h2>Realtime Chat App by DFCH9503</h2>
+        <FaGithub size={28} />
+        <a href="https://github.com/DFCH9503/CHAT-APP">Click to acces the full code!</a>
     </div>
 );
 
