8babaefaa1b5dfa307271ab35950cb6b17a2a1a8
DFCH9503 <dafechaparro@hotmail.com>
2025-05-28 10:57:26 -0500

    adding-3d-library

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Header.scss";
 import { FaGithub } from 'react-icons/fa';
+import { Canvas } from "@react-three/fiber";
+import { Text } from "@react-three/drei";
 
 const Header = () => (
     <div className="header">
-        <h2>Realtime Chat App by DFCH9503</h2>
+        <Canvas>
+            <Text color="black"
+            fontSize={3}
+            position={[0, 0, 0]}>
+                Realtime Chat App by DFCH9503
+            </Text>
+        </Canvas>
         <FaGithub size={28} />
         <a href="https://github.com/DFCH9503/CHAT-APP">Click to acces the full code!</a>
     </div>
